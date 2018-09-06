95f792e49114e84e5e5603358c171b7671915433
Jacob <jacobcrawford1@hotmail.com>
2018-09-06 11:57:56 +0200

    trying safe!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title;
+  data;
+  configUrl;
+  http;
+
+  constructor(http: HttpClient) {
+    this.http = http;
+    this.title = 'First piece of Pi';
+    this.data = {};
+    this.configUrl = 'https://86.52.111.117:20';
+  }
+
+  ngOnInit(): void {
+    this.setData();
+  }
+
+  getData() {
+    return this.data = this.http.get(this.configUrl);
+  }
+
+  setData() {
+    this.getData().subscribe((data: GetDataConfig) => {
+      this.data = data;
+      console.log(this.data);
+    });
+  }
+}
+
+export interface GetDataConfig {
+  let: number;
+  hum: string;
+  temp: string;
 }
